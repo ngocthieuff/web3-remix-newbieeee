d92edb7e5e35495fdca6df06af6d56b435beb88c
ngocthieuff <ptnngoc22@gmail.com>
2022-09-14 14:59:06 +0700

    init client

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # web3-remix-newbieeee
+
+
+```console
+
+npm create vite@latest
+
+// package name: client -> gen folder client
+// React
+// Typescript
+
+cd client
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init
+// https://tailwindcss.com/docs/installation/using-postcss
+// copy postcss.config.cjs manual if it is note auto generated
+```
+
+tailwindcss in react:
+```typescript
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+    </h1>
+```
+
+
+```console
+cd smart_contract
+
+npm init -y //initialize an empty package json
+
+npm install --save-dev hardhat @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers ethers
+
+npx hardhat
+
+npx hardhat test
+
+npm install --save-dev @nomiclabs/hardhat-ethers 'ethers@^5.0.0'
+// usage: deploy.ts
+// import "@nomiclabs/hardhat-ethers";
+// import { ethers } from "hardhat";
+
+
+```
---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import logo from "../../images/logo.png";
+
+const Footer = () => (
+  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+      <div className="flex flex-[0.5] justify-center items-center">
+        <img src={logo} alt="logo" className="w-32" />
+      </div>
+      <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+        <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
+        <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
+        <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
+        <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
+      </div>
+    </div>
+
+    <div className="sm:w-[75%] w-full flex justify-between items-center mt-3">
+      <p className="text-white text-left text-xs">@kryptomastery2022</p>
+      <p className="text-white text-right text-xs">All rights reserved</p>
+    </div>
+  </div>
+);
+
+export default Footer;
